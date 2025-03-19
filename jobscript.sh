824353a74fb48bc84ff063b99a897ad74ebb4f67
Andy M. Celi <andres.munoz01@globant.com>
2025-03-18 22:48:27 -0500

    test trigger wow

--- jobscript.sh
+++ jobscript.sh
@@ -2,9 +2,9 @@
 echo "Bienvenido/a al planeta $planeta"
 if [ "$agente" = "true" ]
 then
-	echo "Prepárese para la misión agente X, que la fuerza te acompañe. modified 22233444"
+	echo "Prepárese para la misión agente X, que la fuerza te acompañe. modified 22233444555"
 else
-	echo "Disfruta el planeta humano/a $nombre, trata de no morir. modified 22233444:"
+	echo "Disfruta el planeta humano/a $nombre, trata de no morir. modified 22233444555"
 fi
 echo "..."
 sleep 10
